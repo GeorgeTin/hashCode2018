522ad2cd22d391ad6b82f560eba20b3033357a2d
tintareanu.george@gmail.com <1githubhashcode>
2018-03-01 19:54:52 +0200

    First classes

---
+++ src/Ride.java
@@ -0,0 +1,45 @@
+import javafx.geometry.Pos;
+
+/**
+ * Created by Alexandra on 3/1/2018.
+ */
+public class Ride {
+
+    private Position startPoz;
+    private Position endPosition;
+
+    private int earliest;
+    private int latest;
+
+    public Position getStartPoz() {
+        return startPoz;
+    }
+
+    public void setStartPoz(Position startPoz) {
+        this.startPoz = startPoz;
+    }
+
+    public Position getEndPosition() {
+        return endPosition;
+    }
+
+    public void setEndPosition(Position endPosition) {
+        this.endPosition = endPosition;
+    }
+
+    public int getEarliest() {
+        return earliest;
+    }
+
+    public void setEarliest(int earliest) {
+        this.earliest = earliest;
+    }
+
+    public int getLatest() {
+        return latest;
+    }
+
+    public void setLatest(int latest) {
+        this.latest = latest;
+    }
+}
