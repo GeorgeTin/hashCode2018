0aea202ffcfc58e2e9d95a2304369825464e10bc
Eli-Ionescu <ionescuelena95@gmail.com>
2018-03-01 21:03:12 +0200

    Reader - tested

--- src/entity/Main.java
+++ src/entity/Main.java
@@ -11,12 +11,12 @@ public class Main {
     public int R, C, F, N, B, T;
     public List<Ride> rides;
 
-    public static void main(String args){
+    public static void main(String[] args){
         Reader reader = new Reader();
         Main main = new Main();
 
         // Test for small input
         reader.readFile("a_example.in", main);
-        System.out.println(main.B);
+
     }
 }
